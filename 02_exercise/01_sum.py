0dc60a08476c0071a9399553ffe20b3bacb4a476
thiggo <thi89248@gmail.com>
2023-07-05 21:46:43 -0300

    create folder 02_exercise and exercise of the sum

---
+++ 02_exercise/01_sum.py
@@ -0,0 +1,4 @@
+number1 = int(input("Informe um número: "))
+number2 = int(input("Informe outro número: "))
+sum = number1 + number2
+print("A soma entre {} e {} é igual a {}.".format(number1,number2,sum))
